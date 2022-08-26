e9e64631c596f3827069897c46d2d444a8319f98
Ginoice Team <team@ginoice.com>
2022-08-26 11:45:30 +0300

    -

--- src/index.ts
+++ src/index.ts
@@ -19,32 +19,32 @@ export default {
   BigNumber
 }
 
-const RPC_URL: string = 'https://rinkeby.infura.io/v3/edefe768418643729b9f1d6194f9d193'
-const NETWORK_ID: string = '0x4'
+// const RPC_URL: string = 'https://rinkeby.infura.io/v3/edefe768418643729b9f1d6194f9d193'
+// const NETWORK_ID: string = '0x4'
 
-const btn:HTMLButtonElement = document.querySelector('.btn-connect') as HTMLButtonElement
+// const btn:HTMLButtonElement = document.querySelector('.btn-connect') as HTMLButtonElement
 
-const wallet:any = new Wallet({
-  RPC_URL: RPC_URL,
-  NETWORK_ID: NETWORK_ID,
-  INFURA_ID: "",
-  hooks: {
-    connecting: res => console.log(res),
-    connectionSuccess: () => console.log('res'),
-    connectionFailed: () => console.log('failed'),
-  }
-})
+// const wallet:any = new Wallet({
+//   RPC_URL: RPC_URL,
+//   NETWORK_ID: NETWORK_ID,
+//   INFURA_ID: "",
+//   hooks: {
+//     connecting: res => console.log(res),
+//     connectionSuccess: () => console.log('res'),
+//     connectionFailed: () => console.log('failed'),
+//   }
+// })
 
-btn.addEventListener('click', () => {
-  wallet.ConnectWallet({
-    connecting: (res: any) => console.log(res),
-    connectionSuccess: (res: any) => console.log(res),
-    connectionFailed: (res:any) => console.log(res)
-  })
-})
+// btn.addEventListener('click', () => {
+//   wallet.ConnectWallet({
+//     connecting: (res: any) => console.log(res),
+//     connectionSuccess: (res: any) => console.log(res),
+//     connectionFailed: (res:any) => console.log(res)
+//   })
+// })
 
-const btnDis:HTMLButtonElement = document.querySelector('.btn-disconect') as HTMLButtonElement
+// const btnDis:HTMLButtonElement = document.querySelector('.btn-disconect') as HTMLButtonElement
 
-btnDis.addEventListener('click', () => {
-  wallet.disconnect()
-})
+// btnDis.addEventListener('click', () => {
+//   wallet.disconnect()
+// })
